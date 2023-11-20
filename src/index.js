b173ae13da567bc5e9bcfbbba0772c7f0bed5b49
WillTms <105085053+WillTms@users.noreply.github.com>
2023-11-19 22:10:17 -0300

    Versão 3.0

--- src/index.js
+++ src/index.js
@@ -9,13 +9,26 @@ const db = require('./db');
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(db);
-
 app.use(router);
 
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname,"../paginas","login.html"));
 });
 
+app.use(express.static("paginas"));
+
 app.listen(3333, ()=>{
     console.log("Servidor pocando")
 });
+
+/*const paginasPath = path.join(__dirname,"../","paginas");
+console.log(paginasPath);
+app.use("/paginas", express.static(paginasPath,{
+    setHeaders:(res, path) => {
+        console.log(path)
+      if (path.endsWith(".css")){
+        res.setHeader("Content-Type","text/css");
+      }
+    },
+}));
+*/
